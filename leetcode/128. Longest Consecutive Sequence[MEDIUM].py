ae344e227ad774ae26c92f54ff8bf9d0583d0e2b
Premkumar <118468266+Premkumar708@users.noreply.github.com>
2024-02-25 08:05:13 +0530

    added file

---
+++ leetcode/128. Longest Consecutive Sequence[MEDIUM].py
@@ -0,0 +1,13 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        sets= set(nums)
+        curr = 0
+        for i in sets:
+            count=1
+            if(i+1 not in sets):
+                j = i-1
+                while(j in sets):
+                    count+=1
+                    j=j-1
+                curr = max(curr,count)
+        return curr 
